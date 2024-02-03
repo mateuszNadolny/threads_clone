d5cb339636e082cedc11e85179ed56b36333be20
mateuszNadolny <mateusznadolny00@gmail.com>
2024-02-03 18:35:30 +0100

    fix: fixed spelling in public routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/nonconsent', '/dashboard', '/api/webhooks']
+  publicRoutes: ['/', '/nonconsent', '/dashboard', '/api/webhook']
 });
 
 export const config = {
