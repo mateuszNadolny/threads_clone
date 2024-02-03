eaf9c5995aa4d582752e0b6374245a5d6a3a67c7
mateuszNadolny <mateusznadolny00@gmail.com>
2024-02-03 18:24:55 +0100

    chore: Added api/webhook route to public routes in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/nonconsent', '/dashboard']
+  publicRoutes: ['/', '/nonconsent', '/dashboard', '/api/webhooks(.*)']
 });
 
 export const config = {
